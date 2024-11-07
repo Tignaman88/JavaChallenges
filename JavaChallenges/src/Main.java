f653661d63b1152127e399e48e76d33e43fda636
Tignaman88 <alessioscarpello2809@gmail.com>
2024-11-07 06:51:42 +0000

    Challenge Fulfilled

--- JavaChallenges/src/Main.java
+++ JavaChallenges/src/Main.java
@@ -1,14 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        BankAccount AlessioScarpello = new BankAccount();
-        AlessioScarpello.setAccountBalance(5.45);
-        AlessioScarpello.setCustomerName("Alessio Scarpello");
-        AlessioScarpello.depositing(1000);
-        AlessioScarpello.withdrawing(2000);
-        BankAccount Mav = new BankAccount();
-        Mav.setAccountBalance(7000);
-        Mav.depositing(300);
-        Mav.withdrawing(3067.99);
+//        BankAccount AlessioScarpello = new BankAccount();
+//        AlessioScarpello.setAccountBalance(5.45);
+//        AlessioScarpello.setCustomerName("Alessio Scarpello");
+//        AlessioScarpello.depositing(1000);
+//        AlessioScarpello.withdrawing(2000);
+//        BankAccount Mav = new BankAccount();
+//        Mav.setAccountBalance(7000);
+//        Mav.depositing(300);
+//        Mav.withdrawing(3067.99);
 
+      SimpleCalculator calculator = new SimpleCalculator();
+      calculator.setFirstNumber(5.0);
+      calculator.setSecondNumber(4);
+      System.out.println(calculator.getAdditionResult());
+      System.out.println(calculator.getSubtractionResult());
+      System.out.println(calculator.getMultiplicationResult());
+      System.out.println(calculator.getDivisionResult());
     }
 }
