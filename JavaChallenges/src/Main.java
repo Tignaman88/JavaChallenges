b88649a70a3f5638ef7eb4e0e4649f4060431470
Tignaman88 <alessioscarpello2809@gmail.com>
2024-11-27 19:57:43 +0000

    Encapsulation Challenge Completed

--- JavaChallenges/src/Main.java
+++ JavaChallenges/src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-//        Player player = new Player();
-//        player.name = "Alessio";
-//        player.health = 200;
-//        player.weapon = "Sword";
-//
-//        int damage = 10;
-//        player.loseHealth(damage);
-//        System.out.println("Remaining health = " + player.healthRemaining());
-//
-//        player.loseHealth(11);
-//        System.out.println("Remaining health = " + player.healthRemaining());
+        Printer printer = new Printer(50, false);
+        System.out.println("Initial page count = " + printer.getPagesPrinted());
 
-        EnhancedPlayer alessio = new EnhancedPlayer("Alessio", 200, "Sword");
-        System.out.println("Initial health is " + alessio.healthRemaining());
+        int pagesPrinted = printer.printPages(5);
+        System.out.printf("Current Job Pages: %d, Printer Total: %d %n",
+                pagesPrinted, printer.getPagesPrinted());
+
+        pagesPrinted = printer.printPages(10);
+        System.out.printf("Current Job Pages: %d, Printer Total: %d %n",
+                pagesPrinted, printer.getPagesPrinted());
     }
 }
